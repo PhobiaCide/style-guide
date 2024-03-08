771cdc5166cf5966acf0d9d12cc6bd0b20912a49
PhobiaCide <aamason86@gmail.com>
2024-03-07 22:02:24 -0600

    * chore(.gitignore): add /html/basic-style-guide copy.html to the gitignore file * style(css/style.css): add support for .eve-sans-neue-condensed and .eve-sans-neue-expanded font families * style(css/style.css): add support for .fragma font family * style(css/style.css): add grid layout for dl element * style(css/style.css): add bold font weight for dt element * style(css/style.css): adjust margin and grid-column-start for dd element * fix(js/mode-switcher.js): rename switchTheme function to toggleMode * fix(js/mode-switcher.js): refactor toggleMode function to use helper functions for extracting mode, defining anti-mode, and making title case

--- js/mode-switcher.js
+++ js/mode-switcher.js
@@ -1,12 +1,12 @@
-function switchTheme(e, mode) {
-  const antiMode = mode === "light" ? "dark" : "light";
-  if (mode === "light" || mode === "dark")
-  {
-    document.documentElement.setAttribute("data-bs-theme", mode);
-    localStorage.setItem("mode", mode);
-    e.setAttribute("disabled", "true");
-    document
-      .getElementById(`${antiMode}-toggle`)
-      .removeAttribute("disabled");
-  }
+const extractMode = (str) => str.split(' ')[0];
+const defineAntiMode = (str) => str.toLowerCase() === "light" ? "dark" : "light";
+const makeTitleCase = (str) => str.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1)).join(' ');
+function toggleMode(e) {
+  const mode = extractMode(e.value);
+  const antiMode = defineAntiMode(mode);
+  document.querySelector("body").setAttribute("data-bs-theme", mode.toLowerCase());
+  localStorage.setItem("mode", mode);
+  e.value = `${makeTitleCase(antiMode)} Mode`;
+  e.classList.remove(`bg-${mode.toLowerCase()}`, `text-${antiMode.toLowerCase()}`, `border-${antiMode.toLowerCase()}`);
+  e.classList.add(`bg-${antiMode.toLowerCase()}`, `text-${mode.toLowerCase()}`, `border-${mode.toLowerCase()}`);
 }
